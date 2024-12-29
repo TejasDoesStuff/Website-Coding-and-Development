157f3456eb0579dff841d8966c098fbbd60375dc
Anirudh Narasimhan <thenoobdude21.12@gmail.com>
2024-12-28 18:56:02 -0800

    idk what this is

--- fbla/app/components/JobCard.tsx
+++ fbla/app/components/JobCard.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 
+<<<<<<< Updated upstream
 const JobCard = ({ user, posting }) => {
+=======
+interface User {
+    name: string;
+    pfp: string;
+}
+
+const JobCard = ({ u, title, description }) => {
+    const username = u.name;
+    const pfp= u.pfp;
+
+>>>>>>> Stashed changes
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg bg-background">
             {imageUrl && (
                 <img
                     className="w-full h-48 object-cover"
+<<<<<<< Updated upstream
                     src={user.pfp}
                     alt={user.name}
                 />
@@ -13,6 +26,15 @@ const JobCard = ({ user, posting }) => {
             <div className="px-6 py-4">
                 <h2 className="font-bold text-xl mb-2 text-text">{posting.title}</h2>
                 <p className="text-text text-base">{posting.description}</p>
+=======
+                    src={pfp}
+                    alt={title}
+                />
+            )}
+            <div className="px-6 py-4">
+                <h2 className="font-bold text-xl mb-2 text-text">{username}</h2>
+                <p className="text-text text-base">{description}</p>
+>>>>>>> Stashed changes
             </div>
         </div>
     );
--- fbla/app/page.tsx
+++ fbla/app/page.tsx
@@ -1,8 +1,14 @@
 import Header from "./Header";
+import JobCard from "./components/JobCard";
 
 export default function Home() {
+
   return (
     <div className="w-screen h-screen">
+<<<<<<< Updated upstream
+=======
+      <Header />
+>>>>>>> Stashed changes
         <JobCard title="React + Tailwind Card"
                  description="This is a sample card component built with React and styled using Tailwind CSS."
                  imageUrl="https://via.placeholder.com/400"/>
