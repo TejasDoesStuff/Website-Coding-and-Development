2b725f4135454454e6da043905c1271461a85e1b
Anirudh Narasimhan <thenoobdude21.12@gmail.com>
2024-12-30 02:22:30 -0800

    made everything 4 indent cuz im a nromal perosn

--- fbla/app/browse/Deck.tsx
+++ fbla/app/browse/Deck.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Row from './Hand'
 
 const Deck = () => {
-  return (
-    <div className = "w-screen h-auto bg-background ">
-        <Row />
-        <Row />
-        <Row />
-    </div>
-  )
+    return (
+        <div className = "w-screen h-auto bg-background ">
+            <Row />
+            <Row />
+            <Row />
+        </div>
+    )
 }
 
 export default Deck
