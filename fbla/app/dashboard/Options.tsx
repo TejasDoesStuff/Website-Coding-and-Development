7df482d5ac5dbbea49a2f6b3fb05987b61fe490f
TejasDoesStuff <tejaspanja@gmail.com>
2025-01-02 16:10:14 -0800

    Finished the layout of the preferences page

--- fbla/app/dashboard/Options.tsx
+++ fbla/app/dashboard/Options.tsx
@@ -38,15 +38,15 @@ export default function Options() {
   }
 
   return (
-    <div className="w-4/5 h-full flex flex-col absolute right-0 overflow-y-scroll">
-      <div className="mx-16 mt-8 ">
+    <div className="w-4/5 h-full flex flex-col absolute right-0 overflow-y-scroll scroll-smooth">
+      <div className="mx-16 mt-8">
         <h1 className="text-5xl font-bold">
           Welcome{" "}
           <span className="underline underline-offset-4">username.</span>
         </h1>
       </div>
-      <div className="m-16 flex flex-col">
-        <h3 className="text-2xl sticky top-8">Profile</h3>
+      <div className="m-16 flex flex-col" id="profile">
+        <h3 className="text-2xl sticky top-0 backdrop-blur border-b border-gray-500 p-6">Profile</h3>
         <div className="m-6 text-md">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
@@ -80,45 +80,59 @@ export default function Options() {
                   </FormItem>
                 )}
               />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="conext@gmail.com" {...field} />
+                    </FormControl>
+                    <FormDescription>This is your email.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <p>Change profile picture here: File upload/Remove pfp</p>
                 <Button type="submit" variant="secondary">
                   Submit
                 </Button>
-                <Button variant="secondary" className="ml-4 bg-red-600">
+                <Button className="ml-4 bg-red-600 text-white hover:bg-red-700">
                   Cancel
                 </Button>
             </form>
           </Form>
         </div>
       </div>
-      <div className="m-16 flex flex-col">
-        <h3 className="text-2xl sticky top-8">Preferences</h3>
+      <div className="m-16 flex flex-col" id="preferences">
+        <h3 className="text-2xl sticky top-0 backdrop-blur-sm p-6 border-b border-gray-500">Preferences</h3>
         <div className="m-6 text-md">
           <p>Preferences go here.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
-          <p>HARHARHARHARHARHARHARHARHARHARHARHAR.</p>
+          <p>They can select preferences for the following</p>
+          <li>Location (Online/In Person - Certain radius from the user's home)</li>
+          <li>Industry (Tags: CS, Med, Engineering, Fast food, etc)</li>
+          <li>Role (Intern, Paid Intern, employee, etc)</li>
+          <li>Salary (Range)</li>
+        </div>
+      </div>
+      <div className="m-16 flex flex-col" id="resume">
+        <h3 className="text-2xl sticky top-0 backdrop-blur-sm p-6 border-b border-gray-500">Resume</h3>
+        <div className="m-6 text-md">
+          <p>Resume goes here.</p>
+          <p>They can upload a file of their resume here</p>
+        </div>
+      </div>
+      <div className="m-16 flex flex-col" id="applications">
+        <h3 className="text-2xl sticky top-0 backdrop-blur-sm p-6 border-b border-gray-500">Applications</h3>
+        <div className="m-6 text-md">
+          <p>You have no pending applications.</p>
+          <p>This is where the user can see/adjust any applications they have currently sent out</p>
+          <p>They can</p>
+          <li>View the job details</li>
+          <li>View/Change their application details</li>
+          <li>View the status of their application</li>
+          <li>Cancel their application</li>
         </div>
       </div>
     </div>
--- fbla/app/dashboard/Selector.tsx
+++ fbla/app/dashboard/Selector.tsx
@@ -2,9 +2,10 @@ export default function Selector() {
   return (
     <div className="w-1/5 h-full border-r border-gray-600 flex sticky">
         <ul className="text-text text-lg my-6 [&>*]:my-2 ml-16 [&>*]:transition-all">
-            <li className="hover:scale-105">Profile</li>
-            <li className="hover:scale-105">Preferences</li>
-            <li className="hover:scale-105">Resume</li>
+            <li className="hover:scale-105"><a href="/dashboard#profile">Profile</a></li>
+            <li className="hover:scale-105"><a href="/dashboard#preferences">Preferences</a></li>
+            <li className="hover:scale-105"><a href="/dashboard#resume">Resume</a></li>
+            <li className="hover:scale-105"><a href="/dashboard#applications">Applications</a></li>
         </ul>
     </div>
   );
